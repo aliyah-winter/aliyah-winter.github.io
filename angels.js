7220a78dd338644e6eb191ed9e0a9e35e0add9cb
aliyah winter <aliyahgwinter@gmail.com>
2022-10-03 11:40:01 +1300

    Create new set loop for random angels placement

--- angels.js
+++ angels.js
@@ -2,24 +2,41 @@
 //create function to randomise a number between 0 and 6
 //create function to insert angel into div
 //loop over img creation and insertion 3 times to place 3 random angels
-//create statement to make sure random nums are never the same
+//create set generator to make sure random nums are never the same
+//use set to update img src
 
 angels = document.getElementById('angels')
 
-function randomNum() {
- return Math.floor(Math.random() * (8 - 1) + 1)
+function randomNumSet() {
+    let set = new Set()
+    while(set.size < 3) {
+        set.add(Math.floor(Math.random() * (8 - 1) + 1))
+    }
+    return set
 }
+let randomSet = randomNumSet()
+console.log(randomSet)
 
-function placeThreeAngel() {
-    for (let i = 0; i < 3; i++) {
-        let randomInt = randomNum()
-        let angelImg = document.createElement('img')
-        angelImg.src = `images/angel${randomInt}.png`
+for (num of randomSet) {
+    let angelImg = document.createElement('img')
+        angelImg.src = `images/angel${num}.png`
         angelImg.setAttribute('class', 'angel')
         angels.appendChild(angelImg)
-    }
 }
-placeThreeAngel()
+
+// function randomNum() {
+//     return Math.floor(Math.random() * (8 - 1) + 1)
+// }
+// function placeThreeAngel() {
+//     for (let i = 0; i < 3; i++) {
+//         let randomInt = randomNum()
+//         let angelImg = document.createElement('img')
+//         angelImg.src = `images/angel${randomInt}.png`
+//         angelImg.setAttribute('class', 'angel')
+//         angels.appendChild(angelImg)
+//     }
+// }
+// placeThreeAngel()
 
 // if (randomInt === randomInt) {
 //     return false
